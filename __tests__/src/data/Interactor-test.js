7e569303104513ece91bc98c5f8454c6cac9465c
yogesh <yogeshkanhurkar@gmail.com>
2021-06-02 10:26:36 +0530

    Remove Garbage files

--- __tests__/src/data/Interactor-test.js
+++
@@ -1,8 +0,0 @@
-import Interactor from '../../../src/data/Interactor';
-import DataSource from '../../../src/data/DataSource';
-
-test('check failure for getJobs', () => {
-    DataSource.getJobIds = jest.fn(() =>);
-});
-
-const jobIds = [27351420, 27347983, 27305250, 27301960];
